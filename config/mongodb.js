415590b4eba2940967532286a00db0c53710a062
Mateus Bretz <mateusbretz@cpejr.com.br>
2024-02-08 19:25:13 -0300

    Adding Database Settings

--- config/mongodb.js
+++ config/mongodb.js
@@ -1,7 +1,13 @@
 const mongoose = require("mongoose");
 
-async function startdb() {
-  await mongoose.connect();
+async function startDB() {
+  try {
+    await mongoose.connect(
+      "mongodb+srv://acervodebomdespacho:hw5UFPX896oI6Ph3@acervobd.io7pudu.mongodb.net/?retryWrites=true&w=majority"
+    );
+    console.log("Banco de dados inicializado");
+  } catch (error) {
+    console.error("Erro ao inicializar o banco de dados:", error);
+  }
 }
-
 module.exports = startDB;
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-// const app = required("./App");
-// const Loaders = require("./config/index");
-// Loaders.start();
-// app.listen(8000, () => console.log("Servidor Rodando!"));
+const startDB = require("../config/mongodb");
+const app = require("./App");
+startDB();
+app.listen(8000, () => console.log("Servidor Rodando!"));
