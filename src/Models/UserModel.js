2496d12e9de5b6be6f04b9c23fd9eac2c6cdbf98
Mateus Bretz <mateusbretz@cpejr.com.br>
2024-02-22 16:24:03 -0300

    feat: adding user models

--- src/Models/UserModel.js
+++ src/Models/UserModel.js
@@ -1,8 +1,49 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
 
 const Schema = mongoose.Schema;
 
-const UserSchema = new Schema({});
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  password: {
+    type: String,
+    required: false,
+    trim: true,
+    select: false,
+  },
+  imageURL: {
+    type: String,
+    required: false,
+    trim: true,
+  },
+  type: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+});
+
+UserSchema.pre("save", async function (next) {
+  const user = this;
+
+  if (user.isModified("password")) {
+    const salt = await bcrypt.genSalt();
+    const hash = await bcrypt.hash(user.password, salt);
+
+    user.password = hash;
+  }
+
+  next();
+});
 
 const UserModel = mongoose.model("users", UserSchema);
 
