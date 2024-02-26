3d894e1b60e2efbfd5fae104dadd350a86bb4fba
g-franklinn <gfranklinn@hotmail.com>
2024-02-26 17:29:18 -0300

    fix: rename routes

--- src/routes/EventRoutes.js
+++ src/routes/EventRoutes.js
@@ -4,9 +4,9 @@ import EventController from "../Controllers/EventController.js";
 
 const eventRoutes = Router();
 
-eventRoutes.get("/event/:id", EventValidator.read, EventController.read);
-eventRoutes.post("/create-event", EventValidator.create, EventController.create);
-eventRoutes.put("/event/:id", EventValidator.update, EventController.update);
-eventRoutes.delete ("/event/:id", EventValidator.destroy, EventController.delete);
+eventRoutes.get("/get/:id", EventValidator.read, EventController.read);
+eventRoutes.post("/create", EventValidator.create, EventController.create);
+eventRoutes.put("/edit/:id", EventValidator.update, EventController.update);
+eventRoutes.delete ("/delete/:id", EventValidator.destroy, EventController.delete);
 
 export default eventRoutes;
