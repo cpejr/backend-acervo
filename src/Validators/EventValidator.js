f0bbd92ede811779e6c6439233b3a314c4173fa0
g-franklinn <gfranklinn@hotmail.com>
2024-02-26 16:19:03 -0300

    feat: create event crud functions

--- src/Validators/EventValidator.js
+++ src/Validators/EventValidator.js
@@ -1,10 +1,37 @@
 import { z } from "zod";
 import { validateRequest } from "zod-express-middleware";
+import mongoose from "mongoose";
 
-const create = validateRequest({});
-const destroy = validateRequest({});
-const update = validateRequest({});
-const read = validateRequest({});
+const create = validateRequest({
+  body: z.object({
+    name: z.string({ required_error: "The name is required" }),
+
+    eventUpload: z.string({ required_error: "Uploading an event is required" }),
+
+    shortDescription: z.string({ required_error: "The short description is required" }),
+
+    longDescription: z.string({ required_error: "The description is required" }),
+  }),
+});
+const destroy = validateRequest({
+  params: z.object({
+    id: z.string().refine(mongoose.isValidObjectId, { message: "The ID is not valid" }),
+  }),
+});
+const update = validateRequest({
+  body: z.object({
+    name: z.string(),
+
+    eventUpload: z.string(),
+
+    shortDescription: z.string(),
+
+    longDescription: z.string(),
+  }),
+});
+const read = validateRequest({
+  body: z.object({}),
+});
 
 export default {
   destroy,
--- src/routes/EventRoutes.js
+++ src/routes/EventRoutes.js
@@ -1,5 +1,12 @@
 import { Router } from "express";
+import EventValidator from "../Validators/EventValidator.js";
+import EventController from "../Controllers/EventController.js";
 
 const eventRoutes = Router();
+
+eventRoutes.get("/event/:id", EventValidator.read, EventController.read);
+eventRoutes.post("/create-event", EventValidator.create, EventController.create);
+eventRoutes.put("/event/:id", EventValidator.update, EventController.update);
+eventRoutes.delete ("/event/:id", EventValidator.destroy, EventController.delete);
 
 export default eventRoutes;
