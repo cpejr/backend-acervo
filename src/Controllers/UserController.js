8f570f0596ab9593caff44c94583ef4d2e37e8f7
Joaovitorleao <97983498+Joaovitorleao@users.noreply.github.com>
2024-03-04 21:05:31 -0300

    feat: model and controller to login

--- src/Controllers/UserController.js
+++ src/Controllers/UserController.js
@@ -18,5 +18,27 @@ class UserController {
     // await UserModel.findByIdAndDelete(id);
     // return res.status(200).json({ mensagem: "Usuário Deletado com sucesso!" });
   }
+  async login(req, res) {
+    try {
+      let userFound = await UserModel.findOne({ email: req.body.email });
+
+      if (!userFound) {
+        userFound = await UserModel.create(req.body);
+
+        await userFound.save();
+      }
+      const token = jwt.sign(
+        {
+          userFound,
+        },
+        process.env.JWT_SECRET,
+        { expiresIn: process.env.JWT_EXPIRE_IN }
+      );
+
+      return res.status(200).json({ token, user: userFound });
+    } catch (error) {
+      res.status(500).json({ message: "Error at login", error: error.message });
+    }
+  }
 }
 export default new UserController();
--- src/Models/UserModel.js
+++ src/Models/UserModel.js
@@ -2,7 +2,29 @@ import mongoose from "mongoose";
 
 const Schema = mongoose.Schema;
 
-const UserSchema = new Schema({});
+const UserSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  imageURL: {
+    type: String,
+    required: false,
+    trim: true,
+  },
+  type: {
+    type: Boolean,
+    require: false,
+    default: false,
+    trim: true,
+  },
+});
 
 const UserModel = mongoose.model("users", UserSchema);
 
