ff87f7df34b8cc79cd9f9dad30002c8ef37d324c
ZeL-ucas <zelucaspomps15@gmail.com>
2024-03-25 09:05:47 -0300

    feat:model start

---
+++ src/Config/Aws.js
@@ -0,0 +1,68 @@
+const S3 = require("aws-sdk/clients/s3");
+const fs = require("fs");
+const connection = require("../database/connection");
+
+const region = process.env.AWS_BUCKET_REGION;
+const accessKeyId = process.env.AWS_ACCESS_KEY;
+const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY;
+const bucketName = process.env.AWS_BUCKET_NAME;
+
+const s3 = new S3({
+  region,
+  accessKeyId,
+  secretAccessKey,
+});
+
+async function uploadAWS(file) {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const fileStream = fs.createReadStream(file.path);
+
+      const uploadParams = {
+        Bucket: bucketName,
+        Body: fileStream,
+        Key: file.filename,
+      };
+
+      const awsRes = await s3.upload(uploadParams).promise();
+
+      resolve(awsRes);
+    } catch (error) {
+      console.log(error);
+      reject(error);
+    }
+  });
+}
+async function getAWS(fileKey) {
+  return new Promise((resolve, reject) => {
+    try {
+      const downloadParams = {
+        Key: fileKey,
+        Bucket: bucketName,
+      };
+      resolve(s3.getObject(downloadParams).createReadStream());
+    } catch (error) {
+      console.log(error);
+      reject(error);
+    }
+  });
+}
+async function deleteAWS(fileKey) {
+  return new Promise((resolve, reject) => {
+    try {
+      const deleteParams = {
+        Bucket: bucketName,
+        Key: fileKey,
+      };
+
+      resolve(
+        s3.deleteObject(deleteParams, (err, data) => {
+          if (err) console.log("Erro no delete!", err);
+        })
+      );
+    } catch (error) {
+      console.log(error);
+      reject(error);
+    }
+  });
+}
