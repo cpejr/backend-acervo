565906e7fa34637b30c28c95f8539336db1f500d
SamuelFVG <samuelfelipeif@gmail.com>
2024-03-28 12:54:34 -0300

    fix: change the order of configs in app.js

--- src/App.js
+++ src/App.js
@@ -6,12 +6,12 @@ import bodyParser from "body-parser";
 
 dotenv.config();
 const app = express();
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use(cors());
-app.use(routes);
 app.use(bodyParser.json({ limit: "100mb" }));
 app.use(express.urlencoded({ limit: "100mb", extended: true }));
+app.use(express.json());
+app.use(cors());
+
+app.use(routes);
 
 // Non existing routes
 app.use("*", (req, res, next) => {
